700d731ad5f975cb5780471b6ca2790863a54531
ronaksharmark77 <ronaksharma.rk77@gmail.com>
2024-08-24 00:57:45 +0530

    database corrections

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -72,6 +72,7 @@ export async function GET() {
 
         return NextResponse.json({
             message: "Ok",
+            updatedProducts
         });
     } catch (error: any) {
         console.error(`Error during GET /api/cron: ${error.message}`);
